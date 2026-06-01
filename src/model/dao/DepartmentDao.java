06edf8fe9fb5e4d96e73210263c47c25474e9abd
Andre Cano <andrepcano@gmail.com>
2026-06-01 18:14:17 -0300

    feat: criando interfaces SellerDao e DepartmentDao

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import com.mysql.cj.xdevapi.Client;
+import model.entities.Department;
+
+import java.util.List;
+import java.util.Objects;
+
+public interface DepartmentDao {
+
+    void Insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
