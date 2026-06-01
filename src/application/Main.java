d50b45559f35c38d6ea48d87f2cb33dd608fc220
Andre Cano <andrepcano@gmail.com>
2026-06-01 18:38:32 -0300

    feat: criando findById implementação

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,12 +12,9 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Department obj = new Department(5, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(2, "André", "andre@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
