5705475a84f96f83dbd161b3f767a052a0fcb683
Andre Cano <andrepcano@gmail.com>
2026-06-01 18:04:43 -0300

    feat: criando Department entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,13 @@
+package application;
+
+import model.entities.Department;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Department obj = new Department(5, "Books");
+        System.out.println(obj);
     }
 }
