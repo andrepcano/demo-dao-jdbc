135dfde33fcd8cd7c95c504518e9bc4d52c34acb
Andre Cano <andrepcano@gmail.com>
2026-06-01 21:50:12 -0300

    feat: instanciando findById

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,7 +1,74 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
+import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.*;
+import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
 
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        try {
+            pst = conn.prepareStatement("SELECT * FROM department WHERE id = ?", Statement.RETURN_GENERATED_KEYS);
+
+            pst.setInt(1, 2);
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                Department dep = instantiateDepartment(rs);
+                return dep;
+            }
+            return null;
+
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(pst);
+            DB.closeResultSet(rs);
+        }
+
+
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("Id"));
+        dep.setName(rs.getString("Name"));
+        return dep;
+    }
 }
